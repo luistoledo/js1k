90fefd14d04eac24ced8a2026f11a8253da8461b
Luis Toledo <luisernesto.toledo@gmail.com>
2014-03-09 12:58:55 -0600

    embed images, and scale

--- js1k.js
+++ js1k.js
@@ -2,8 +2,11 @@
 // c = canvas context
 
 d = document;
-W = a.width;
-H = a.height;
+
+zm=3;
+
+W = a.width  / zm;
+H = a.height / zm;
 
 //KEY
 k = 0;
@@ -16,26 +19,51 @@ z = 20;
 //CUBES HEIGHT/WIDTH, PLAYER HEIGHT 
 //MMM.. COLORS
 g = 'gray';
-r = 'red';
+r = '#F5C959';
 b = 'black';
 
 // DELTATIMES
 N = Date.now;
 pN = N();
-x=10;
+
+// INIT
+x=10, y=10;
+
+c.scale(zm,zm);
+
+var cs = Math.cos(120.01), sn = Math.sin(120.01);
+var hh = Math.cos(17.50);
+var aa = 100*cs, bb = -100*sn, cc = 200;
+var dd = hh*100*sn, ee = hh*100*cs, ff = 200;
+
+var image = new Image();
+image.src = "http://localhost:8000/grasstile.jpg";
+
+
+var img = new Image();
+img.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAADFBMVEUAAABrw/85jv9Rtv+Mhd5YAAAAAXRSTlMAQObYZgAAAGZJREFUeNqt00EKwCAQQ9Ek3v/OtSBB0WDR/m0es1L8HGs3QE3lvbxFIANeg7UQGggnJMEAXIPSQgKOnIT6CyfAuwEDsNiCUegaeA9Ch4BbEHaLHbCQAVrfAQOwGN8ruqbfUQFOewAGOgOIVNucnAAAAABJRU5ErkJggg==";
+
+//c.drawImage(this, 0, 0,
+
+
+//c.setTransform(aa, dd, bb, ee, cc, ff);
+//tx=-5;ty=-5;
 
 // GAMELOOP
 function gl() {
     requestAnimationFrame(gl);
-    dr(0,0,W,H,'#fff');
-    
-    D = (N()-pN) / 10;
+    dr(0,0,W,H,'#5E5E5E');
+        
+    D = (N()-pN) / 100;
     pN = N();
 
-    x = x + 2;
+    x += 2;
+    y = 50*Math.cos(x);
     if (x>W) { x=0; }
-    
-    dr(x,10,10,10,r);
+
+    c.drawImage(img,0,0);    
+    dr(x,y,10,10,r);
+
 };
 gl();
 
